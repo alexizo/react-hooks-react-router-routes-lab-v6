446ac5d3060218a2083894535ef3faded9b50bb8
Alex Mukuha <alex.mukuha@student.moringaschool.com>
2024-08-20 14:10:09 +0300

    done

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,35 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then(response => response.json())
+      .then(data => setMovie(data));
+  }, [id]);
+
+  if (!movie) return <p>Loading...</p>;
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
+        <h1>{movie.title}</h1>
       </header>
       <main>
-        {/* Movie info here! */}
+        <p>Runtime: {movie.time} minutes</p>
+        <div>
+          {movie.genres.map(genre => (
+            <span key={genre}>{genre}</span>
+          ))}
+        </div>
       </main>
     </>
   );
-};
+}
 
 export default Movie;
